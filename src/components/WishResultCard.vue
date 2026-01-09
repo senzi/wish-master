91d7c2e8192dd9d3ec4b8f2258f905b0a3a8b00e
senzi <sennes111762@gmail.com>
2026-01-09 16:57:41 +0800

    🔧 chore(WishResultCard): update wish count display
    - update number from "120万" to "151万" to reflect new wish count

--- src/components/WishResultCard.vue
+++ src/components/WishResultCard.vue
@@ -134,7 +134,7 @@ onMounted(async () => {
 
     <div class="stats-hint">
       <p>
-        完美许愿器已经处理超过 <span class="num">120万</span> 个愿望。
+        完美许愿器已经处理超过 <span class="num">151万</span> 个愿望。
       </p>
       <p>
         如果你因此感到“快乐”，可以点击页面下方的按钮支持我w
